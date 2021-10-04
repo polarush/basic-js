5b4a2237b72ff4343525612ff5c846e2c1d718fe
polarus <polyarush.e@mail.ru>
2021-10-05 01:31:25 +0400

    feat: complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,104 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(input, key) {
+
+    if (!input) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (!key) { 
+      throw new Error('Incorrect arguments!'); 
+    }
+
+    input = input.toUpperCase();
+    key = key.repeat(Math.ceil(input.length/key.length)).toUpperCase();
+
+    let firstChar = 65
+    let alphabetLen = 26;
+
+    let res = [];
+
+      for (let i = 0; i < input.length; i++) {
+        if (input[i].charCodeAt(0) < 65) {
+          res.push(input[i]);
+          input = input.replace(input[i], '');
+          i--;
+
+        } 
+        else if (input[i].charCodeAt(0) > 90) {
+          res.push(input[i]);
+          input = input.replace(input[i], '');
+          i--;
+
+        }
+        else {
+          let letterId = input.charCodeAt(i) - firstChar;
+          let shift = key.charCodeAt(i) - firstChar;
+          res.push(String.fromCharCode(firstChar + (letterId + shift) % alphabetLen));
+        }
+
+      }
+
+    if (this.direct) {
+      return res.join('')
+    }
+    else {
+      return res.reverse().join('');
+    }
+  }
+
+  decrypt(input, key) {
+    if (!input) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (!key) { 
+      throw new Error('Incorrect arguments!'); 
+    }
+
+    input = input.toUpperCase();
+    key = key.repeat(Math.ceil(input.length/key.length)).toUpperCase();
+
+    let firstChar = 65
+    let alphabetLen = 26;
+
+    let res = [];
+
+      for (let i = 0; i < input.length; i++) {
+        if (input[i].charCodeAt(0) < 65) {
+          
+          res.push(input[i]);
+          input = input.replace(input[i], '');
+          i--;
+        
+        }
+        else if (input[i].charCodeAt(0) > 90) {
+          res.push(input[i]);
+          input = input.replace(input[i], '');
+          i--;
+        
+        } 
+        else {
+        
+          let letterId = input.charCodeAt(i) - firstChar;
+          let shift = key.charCodeAt(i) - firstChar;
+          res.push(String.fromCharCode(firstChar + (letterId - shift + alphabetLen) % alphabetLen));
+        
+        }
+      }
+
+    if (this.direct) {
+      return res.join('')
+    }
+    else {
+      return res.reverse().join('');
+    }
+
+
   }
 }
